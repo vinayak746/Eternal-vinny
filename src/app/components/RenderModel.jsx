6c3b04230304ce5d8b637aff35b7f3d163c81815
Vinayak Arora <vinayakarora7461@gmail.com>
2025-10-23 10:43:20 +0530

    First Commit

---
+++ src/app/components/RenderModel.jsx
@@ -0,0 +1,8 @@
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+
+const RenderModel = ({ children, className }) => {
+  return <Canvas></Canvas>;
+};
+
+export default RenderModel;
