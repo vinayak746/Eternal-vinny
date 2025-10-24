f9e89e2a0b2d361d371016e2be0c0fe4ad93e6d2
Vinayak Arora <vinayakarora7461@gmail.com>
2025-10-24 13:53:12 +0530

    feat: update project data and enhance CSS variables for improved theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,9 +23,10 @@ module.exports = {
           "radial-gradient(50% 50% at 50% 50%, rgba(173, 216, 230, 5) 0%, rgba(217, 217, 217, 0) 100%)",
       },
       boxShadow: {
-        "glass-sm": "0 0 30px 5px rgba(254,254,91,0.7)",
-        "glass-hover": "0 0 60px 20px rgba(254,254,91,0.9)",
+        "glass-sm": "0 0 30px 5px rgba(255, 255, 255, 0.8)", // soft, bright white glass glow
+        "glass-hover": "0 0 60px 20px rgba(255, 255, 255, 1.0)", // intense, fully luminous white glow on hover
       },
+
       keyframes: {
         "spin-reverse": {
           "0%": { transform: "rotate(0deg)" },
