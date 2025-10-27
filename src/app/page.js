c13d45071ef94d17b8365e81093ace7859319807
Vinayak Arora <vinayakarora7461@gmail.com>
2025-10-27 14:42:26 +0530

    Increased the quality of the home background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import bg from "../../public/background/home-new.png";
+import bg from "../../public/background/home-newer.png";
 import ShadowSlave from "./components/models/ShadowSlave";
 import RenderModel from "./components/RenderModel";
 import Navigation from "./components/navigation";
